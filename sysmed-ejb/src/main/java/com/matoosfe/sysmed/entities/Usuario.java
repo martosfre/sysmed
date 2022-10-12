3fadefccf4de35bd4400c9e09ff81a12cdaea1e3
martosfre <mtoscano@matoosfe.com>
2022-10-12 18:44:50 -0500

    Implementación Pantalla Login

---
+++ sysmed-ejb/src/main/java/com/matoosfe/sysmed/entities/Usuario.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.matoosfe.sysmed.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author martosfre
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class Usuario {
+   private String nombreUsu;
+   private String claveUsu;
+}
---
+++ sysmed-web/src/main/java/com/matoosfe/sysmed/beans/LoginBean.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.matoosfe.sysmed.beans;
+
+import com.matoosfe.sysmed.entities.Usuario;
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author martosfre
+ */
+@Named
+@SessionScoped
+public class LoginBean implements Serializable{
+    @Getter @Setter
+    private Usuario usuario;
+
+    public LoginBean() {
+        this.usuario = new Usuario(); //Registro
+    }
+    
+    /**
+     * Método para validar el usuario
+     */
+    public void validarUsuario(){
+         FacesMessage mensajeJSF = new FacesMessage();
+
+        if (usuario.getNombreUsu().equals("admin") && usuario.getClaveUsu().equals("1234")) {
+            //Mensaje de Información
+            mensajeJSF.setSummary("Usuario correcto, bienvenido!!");
+            mensajeJSF.setSeverity(FacesMessage.SEVERITY_INFO);
+            FacesContext.getCurrentInstance().addMessage(null, mensajeJSF);
+            //Redireccionar
+        } else {
+            //Mensaje de Error
+            mensajeJSF.setSummary("Credenciales Incorrectas");
+            mensajeJSF.setSeverity(FacesMessage.SEVERITY_ERROR);
+            FacesContext.getCurrentInstance().addMessage(null, mensajeJSF);
+            //Redireccionar
+        }
+    }
+    
+}
